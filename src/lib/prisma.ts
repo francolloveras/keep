2628f87215365a397b0a5f3e2adf7158f8ac6efa
Franco Lloveras <francorlloveras@gmail.com>
2025-01-15 19:33:18 -0300

    Add Prisma ORM, add user and session models, create Prisma Client

---
+++ src/lib/prisma.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from '@prisma/client'
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined
+}
+
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
+
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
+}
