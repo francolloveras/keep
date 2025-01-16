73a0a975c5cfc80cbf7f6ed8f294ab5372678bf6
Franco Lloveras <francorlloveras@gmail.com>
2025-01-15 21:13:58 -0300

    Create custom useContext hook

---
+++ src/lib/hooks/useContext.ts
@@ -0,0 +1,12 @@
+'use client'
+import { type Context, useContext as ReactUseContext } from 'react'
+
+export const useContext = <ItemType>(context: Context<ItemType | null>): ItemType => {
+  const contextValue = ReactUseContext(context)
+
+  if (!contextValue) {
+    throw Error('Context has not been Provided.')
+  }
+
+  return contextValue
+}
