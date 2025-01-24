f3fa639db925478310fba0b060868d662ca672ab
Franco Lloveras <francorlloveras@gmail.com>
2025-01-23 23:23:52 -0300

    Improve color scheme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,18 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        rubik: ['var(--font-rubik)']
+        inter: ['var(--font-inter)']
+      },
+      colors: {
+        primary: 'rgb(var(--primary) / <alpha-value>)',
+        error: 'rgb(var(--error) / <alpha-value>)',
+        background: 'rgb(var(--background) / <alpha-value>)',
+        text: 'rgb(var(--text) / <alpha-value>)',
+        surface: 'rgb(var(--surface) / <alpha-value>)',
+        outline: 'rgb(var(--outline) / <alpha-value>)'
+      },
+      backgroundColor: {
+        'current-shadow': 'color-mix(in srgb, currentColor 5%, transparent)'
       }
     }
   },
