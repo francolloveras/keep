7296e181feebdf7dc0c374e04b7b049fe55be44c
Franco Lloveras <francorlloveras@gmail.com>
2025-01-23 23:26:56 -0300

    Improve Locale switcher using new modal API and adding flags icons

--- src/components/ui/locale-switcher.tsx
+++ src/components/ui/locale-switcher.tsx
@@ -1,15 +1,15 @@
 'use client'
 
+import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
-import { LOCALES, LOCALES_LONG } from '@/lib/const'
+import Icon from '@/components/ui/icon'
+import Menu from '@/components/ui/menu'
+import { LOCALES, LOCALES_FLAGS, LOCALES_LONG } from '@/lib/const'
 import { Locale } from '@/lib/dictionaries/get-dictionaries'
-import { useModal } from '@/lib/hooks/useModal'
-import Icon from './icon'
 
 export default function LocaleSwitcher({ lang }: { lang: Locale }) {
-  const { isOpen, modalRef, toggleModal } = useModal(false)
   const pathname = usePathname()
 
   const redirectedPathname = (locale: Locale) => {
@@ -21,35 +21,41 @@ export default function LocaleSwitcher({ lang }: { lang: Locale }) {
     return segments.join('/')
   }
 
+  const longLocale = LOCALES_LONG[lang]
+
   return (
-    <div className="relative">
-      <button
-        onClick={toggleModal}
-        className="hover:bg-zinc-800 hover:text-white rounded-lg px-3 py-1.5"
-      >
-        <Icon icon="faGlobe" className="mr-3 size-3.5" />
-        {LOCALES_LONG[lang]}
-        <Icon icon="faChevronDown" className="ml-2 size-2.5" />
-      </button>
-      {isOpen && (
-        <ul
-          ref={modalRef}
-          className="absolute mt-1 overflow-hidden w-full border-zinc-600 border rounded-lg bg-zinc-900"
-        >
-          {LOCALES.map((locale) => {
-            return (
-              <li key={locale}>
-                <Link
-                  href={redirectedPathname(locale)}
-                  className="block py-1.5 px-3 cursor-pointer hover:text-white hover:bg-zinc-800"
-                >
-                  {LOCALES_LONG[locale]}
-                </Link>
-              </li>
-            )
-          })}
-        </ul>
-      )}
-    </div>
+    <Menu
+      id="locale-switcher"
+      label={
+        <>
+          <Icon icon="faEarthAmericas" className="mr-3" />
+          <span className="inline-block w-16 text-left">{longLocale}</span>
+          <Icon icon="faChevronDown" className="ml-2 size-2.5" />
+        </>
+      }
+      className="rounded-md px-3 py-1.5 hover:bg-current-shadow"
+    >
+      <ul className="mt-1 w-full overflow-hidden rounded-md border border-outline bg-background shadow-sm">
+        {LOCALES.map((locale) => {
+          return (
+            <li key={locale}>
+              <Link
+                href={redirectedPathname(locale)}
+                className="flex cursor-pointer items-center gap-x-3 px-3 py-1.5 hover:bg-current-shadow"
+              >
+                <Image
+                  src={LOCALES_FLAGS[locale]}
+                  alt={longLocale}
+                  width={16}
+                  height={12}
+                  className="rounded-sm"
+                />
+                {LOCALES_LONG[locale]}
+              </Link>
+            </li>
+          )
+        })}
+      </ul>
+    </Menu>
   )
 }
