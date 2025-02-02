76645c83040ce1bb2ae3f54a421b58abb5089e92
Franco Lloveras <francorlloveras@gmail.com>
2025-02-01 22:27:45 -0300

    Add accents colors and change Toast UI styles

--- src/components/ui/toast-wrapper.tsx
+++ src/components/ui/toast-wrapper.tsx
@@ -1,10 +1,12 @@
 'use client'
 
 import Icon, { type Icons } from '@/components/ui/icon'
+import { useLocale } from '@/lib/hooks/useLocale'
 import { useToast } from '@/lib/hooks/useToast'
 import { cx } from '@/lib/utils'
 
 export default function ToastWrapper() {
+  const { dict } = useLocale()
   const { toasts, deleteToast } = useToast()
 
   const defaultIcons: Record<string, Icons> = {
@@ -17,34 +19,67 @@ export default function ToastWrapper() {
   return (
     <div
       role="alert"
-      className="fixed bottom-5 left-2/4 z-50 flex -translate-x-2/4 flex-col items-center justify-center gap-2"
+      className="fixed bottom-5 left-2/4 z-50 flex -translate-x-2/4 flex-col items-center justify-center gap-4"
     >
       {toasts.map(({ id, type, message, icon, isVisible, action }) => (
         <div
           key={id}
           className={cx(
-            'flex w-fit items-center justify-between gap-8 rounded-md px-5 py-2 shadow-md transition-all duration-300 ease-in-out',
+            'relative flex w-fit justify-between bg-background border border-outline gap-10 rounded-md px-5 py-2 shadow-lg shadow-black/5 transition-all duration-300 ease-in-out',
             {
-              'bg-green-400 text-green-900': type === 'success',
-              'bg-red-400 text-red-900': type === 'error',
-              'bg-blue-400 text-blue-900': type === 'info',
-              'bg-yellow-400 text-yellow-900': type === 'warning',
               'translate-y-0 opacity-100': isVisible,
               'translate-y-5 opacity-0': !isVisible
             }
           )}
         >
-          <p className="space-x-1 text-nowrap">
-            <Icon icon={icon ?? defaultIcons[type]} />
-            <span>{message}</span>
-          </p>
+          <div
+            className={cx('absolute top-0 rounded-l-md left-0 h-full w-2', {
+              'bg-success': type === 'success',
+              'bg-error': type === 'error',
+              'bg-info': type === 'info',
+              'bg-warning': type === 'warning'
+            })}
+          />
+          <div className="flex flex-1 gap-x-2">
+            <div>
+              <Icon
+                className={cx('mb-0', {
+                  'text-success': type === 'success',
+                  'text-error': type === 'error',
+                  'text-info': type === 'info',
+                  'text-warning': type === 'warning'
+                })}
+                icon={icon ?? defaultIcons[type]}
+              />
+            </div>
+            <div className="space-y-1">
+              <h3>{dict.general.accents[type]}</h3>
+              <p className="text-nowrap text-text/80">{message}</p>
+            </div>
+          </div>
           <div className="space-x-px">
             {action && (
-              <button className="px-2 text-xs uppercase" onClick={action.function}>
+              <button
+                className={cx('rounded-md px-2 py-1.5 text-xs uppercase hover:bg-current-shadow', {
+                  'text-success': type === 'success',
+                  'text-error': type === 'error',
+                  'text-info': type === 'info',
+                  'text-warning': type === 'warning'
+                })}
+                onClick={action.function}
+              >
                 {action.label}
               </button>
             )}
-            <button onClick={() => deleteToast(id)}>
+            <button
+              className={cx('rounded-md p-1 px-1.5 hover:bg-current-shadow', {
+                'text-success': type === 'success',
+                'text-error': type === 'error',
+                'text-info': type === 'info',
+                'text-warning': type === 'warning'
+              })}
+              onClick={() => deleteToast(id)}
+            >
               <Icon icon="faXmark" />
             </button>
           </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,9 @@ export default {
       colors: {
         primary: 'rgb(var(--primary) / <alpha-value>)',
         error: 'rgb(var(--error) / <alpha-value>)',
+        success: 'rgb(var(--success) / <alpha-value>)',
+        warning: 'rgb(var(--warning) / <alpha-value>)',
+        info: 'rgb(var(--info) / <alpha-value>)',
         background: 'rgb(var(--background) / <alpha-value>)',
         text: 'rgb(var(--text) / <alpha-value>)',
         surface: 'rgb(var(--surface) / <alpha-value>)',
